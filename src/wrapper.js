842a61f5efe6f4571304c6cba96877c0314c578e
gauravprwl14 <gauravprwl14@gmail.com>
2018-05-28 15:18:47 +0530

    importing the core.js, websocket.js and bosh.js

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,16 +1,22 @@
-(function(root){
-    if(typeof define === 'function' && define.amd){
-        define([
-            "strophe-core",
-            "strophe-bosh",
-            "strophe-websocket"
-        ], function (wrapper) {
-            return wrapper;
-        });
-    } else if (typeof exports === 'object') {
-        var core = require('./core');
-        require('./bosh');
-        require('./websocket');
-        module.exports = core;
-    }
-})(this);
+// (function(root){
+//     if(typeof define === 'function' && define.amd){
+//         define([
+//             "strophe-core",
+//             "strophe-bosh",
+//             "strophe-websocket"
+//         ], function (wrapper) {
+//             return wrapper;
+//         });
+//     } else if (typeof exports === 'object') {
+//         var core = require('./core');
+//         require('./bosh');
+//         require('./websocket');
+//         module.exports = core;
+//     }
+// })(this);
+
+var core = require("./core");
+require("./bosh");
+require("./websocket");
+
+module.exports = core;
